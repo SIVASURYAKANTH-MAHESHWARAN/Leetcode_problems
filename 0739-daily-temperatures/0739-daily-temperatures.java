65a236de39d35a8287c205986b87eb468cda275d
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-02-18 06:34:49 +0530

    Time: 63 ms (44.57%), Space: 107.7 MB (44.64%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,35 +1,17 @@
 class Solution {
-    class pair{
-        int ind;
-        int cnt;
-        pair(int ind,int cnt){
-            this.ind=ind;
-            this.cnt=cnt;
-        }
-    }
     public int[] dailyTemperatures(int[] temp) {
+        int[]arr=Arrays.copyOf(temp,temp.length);
         int len=temp.length;
-        int[]arr=new int[len];
-        int i=0;
-        Stack<pair>stk=new Stack<>();
-        Map<Integer,Integer>map=new HashMap<>();
-        
-        while(i<len){
-            int cnt=1;
-            while(!stk.isEmpty() && temp[stk.peek().ind]<temp[i]){
-                pair p=stk.pop();
-                map.put(p.ind,cnt);
-                cnt+=p.cnt;
+        Stack<Integer>stk=new Stack<>();
+        for(int i=0;i<len;i++){
+            while(!stk.isEmpty() && temp[i]>temp[stk.peek()]){
+                int ind=stk.pop();
+                arr[ind]=i-ind;
             }
-            pair p=new pair(i,cnt);
-            stk.push(p);
-            i++;
+            stk.push(i);
         }
         while(!stk.isEmpty()){
-            map.put(stk.pop().ind,0);
-        }
-        for(i=0;i<len;i++){
-            arr[i]=map.get(i);
+            arr[stk.pop()]=0;
         }
         return arr;
     }
