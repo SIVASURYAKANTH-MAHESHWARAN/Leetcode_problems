20415c976679f0fccbe4666faef90fe9137dfebc
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-02-18 06:44:15 +0530

    Time: 4 ms (53.3%), Space: 45.6 MB (23.58%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,26 +1,14 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int len=prices.length;
-        int[]arr=new int[len];
+        int[]arr=Arrays.copyOf(prices,len);
         Stack<Integer>stk=new Stack<>();
-        int i=0;
-        
-        Map<Integer,Integer>map=new HashMap<>();
-        while(i<len){
+        for(int i=0;i<len;i++){
             while(!stk.isEmpty() && prices[stk.peek()]>=prices[i]){
                 int ind=stk.pop();
-                map.put(ind,prices[ind]-prices[i]);
+                arr[ind]=arr[ind]-arr[i];
             }
             stk.push(i);
-            i++;
-        }
-        while(!stk.isEmpty()){
-            int ind=stk.pop();
-            map.put(ind,prices[ind]);
-        }
-        i=0;
-        for(i=0;i<len;i++){
-            arr[i]=map.get(i);
         }
         return arr;
     }
