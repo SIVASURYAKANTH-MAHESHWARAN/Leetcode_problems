7382021f43120141314cd24ae9accd4a60afa5d6
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-02-26 21:42:27 +0530

    Time: 33 ms (31.28%), Space: 152.4 MB (29.76%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len=nums.length;
+        int[]arr=new int[len-k+1];
+        Deque<Integer>deck=new LinkedList<>();
+        for(int i=0;i<len;i++){
+            if(!deck.isEmpty() && deck.peekFirst()<=i-k){
+                deck.pollFirst();
+            }
+            while(!deck.isEmpty() && nums[deck.peekLast()]<=nums[i]){
+                deck.pollLast();
+            }
+            deck.offerLast(i);
+            if(i>=k-1){
+                // System.out.println((i-k)+1);
+                arr[(i-k)+1]=nums[deck.peekFirst()];
+            }
+        }
+        return arr;
+    }
+}
