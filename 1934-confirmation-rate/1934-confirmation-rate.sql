598600753c4af314654f72985569fd0243f34a04
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-05-13 16:37:28 +0530

    Time: 792 ms (14.07%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,13 +1,18 @@
 # Write your MySQL query statement below
--- select t.user_id,round(count(c.user_id)/t.counts) from Signups as s left join (select s.user_id,count(c.user_id) as counts,c.action from Signups as s left join Confirmations as c on s.user_id=c.user_id group by c.user_id) as t join Confirmations as c on t.user_id=c.user_id where c.action='confirmed' group by c.user_id;
 
+select s.user_id,round(avg(case when c.action='confirmed' then 1 else 0 end),2) as confirmation_rate from Signups as s left join Confirmations as c on s.user_id=c.user_id group by s.user_id; 
 
-select t.user_id,round((case when (counts2/counts1) is null then 0 else (counts2/counts1)end),2) as confirmation_rate from (select s.user_id,count(c.user_id) as counts1 from Signups as s left join Confirmations as c on s.user_id=c.user_id group by s.user_id) as t join (SELECT s.user_id, COUNT(c.action) as counts2 FROM Signups as s LEFT JOIN Confirmations as c ON s.user_id = c.user_id AND c.action = 'confirmed' GROUP BY s.user_id) as s on t.user_id=s.user_id;
+-- select t.user_id,round((case when (counts2/counts1) is null then 0 else (counts2/counts1)end),2) as confirmation_rate from (select s.user_id,count(c.user_id) as counts1 from Signups as s left join Confirmations as c on s.user_id=c.user_id group by s.user_id) as t join (SELECT s.user_id, COUNT(c.action) as counts2 FROM Signups as s LEFT JOIN Confirmations as c ON s.user_id = c.user_id AND c.action = 'confirmed' GROUP BY s.user_id) as s on t.user_id=s.user_id;
 
 -- (select s.user_id,count(c.user_id) from Signups as s left join Confirmations as c on s.user_id=c.user_id group by s.user_id)
 
 
 -- (SELECT s.user_id, COUNT(c.action) as counts2 FROM Signups as s LEFT JOIN Confirmations as c ON s.user_id = c.user_id AND c.action = 'confirmed' GROUP BY s.user_id)
+
+
+
+
+-- select s.user_id,count(c.action) from Signups as s left join Confirmations as c on s.user_id=c.user_id where c.action is null or c.action='confirmed' group by s.user_id; 
 
 -- SELECT 
 --     s.user_id,
@@ -19,4 +24,3 @@ select t.user_id,round((case when (counts2/counts1) is null then 0 else (counts2
 
 
 
--- select s.user_id,count(c.action) from Signups as s left join Confirmations as c on s.user_id=c.user_id where c.action is null or c.action='confirmed' group by s.user_id; 
