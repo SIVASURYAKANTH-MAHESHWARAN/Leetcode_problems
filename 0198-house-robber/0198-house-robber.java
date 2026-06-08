92ed73ff8443bff13469eae5a15a32f4610093e5
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-06-08 12:03:07 +0530

    Time: 12 ms (0.54%), Space: 43 MB (5.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,15 @@
 class Solution {
-    public int recurr(int[]nums,int len,int ind,int[]dp){
-        if(ind>=len){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int take=nums[ind]+recurr(nums,len,ind+2,dp);
-        int not_take=recurr(nums,len,ind+1,dp);
-        return dp[ind]=Math.max(take,not_take);
-    }
     public int rob(int[] nums) {
         int len=nums.length;
-        int[]dp=new int[len];
-        Arrays.fill(dp,-1);
-        return recurr(nums,len,0,dp);
+        int[]dp=new int[len+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=len;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        for(int i=0;i<=len;i++){
+            System.out.print(dp[i]+" ");
+        }
+        return dp[len];
     }
 }
