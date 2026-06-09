0222e7aac6b97e0bf98193daef2e9fb5192af90c
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-06-09 11:01:37 +0530

    Time: 26 ms (28.41%), Space: 47.1 MB (23.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,35 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
-        int[]dp=new int[amount+1];
-       
-        Arrays.fill(dp,Integer.MAX_VALUE);
-         dp[0]=0;
-        for(int j=0;j<coins.length;j++){
-            int coin=coins[j];
-            for(int i=coin;i<=amount;i++){
-                if(dp[i-coin]!=Integer.MAX_VALUE){
-                    dp[i]=Math.min(dp[i-coin]+1,dp[i]);
-                    
-                }
-                
+    public int recurr(int ind,int[]coins,int amt,int[][]dp){
+        if(ind==0){
+            if(amt%coins[0]==0){
+                return amt/coins[0];
+            }
+            else{
+                return Integer.MAX_VALUE;
             }
         }
-        return (dp[amount]==Integer.MAX_VALUE)?-1:dp[amount];
+        if(dp[ind][amt]!=-1){
+            return dp[ind][amt];
+        }
+        int not_take=recurr(ind-1,coins,amt,dp);
+        int take=Integer.MAX_VALUE;
+        if(coins[ind]<=amt){
+            int takes=recurr(ind,coins,amt-coins[ind],dp);
+            if(takes!=Integer.MAX_VALUE){
+                take=1+takes;
+            }
+        }
+        return dp[ind][amt]=Math.min(not_take,take);
+    }
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int[][]dp=new int[coins.length][amount+1];
+        for(int i=0;i<coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int ans=recurr(coins.length-1,coins,amount,dp);
+        return (ans==Integer.MAX_VALUE)?-1:ans;   
     }
 }
