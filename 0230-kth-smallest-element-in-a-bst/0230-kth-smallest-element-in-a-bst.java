62616681628d09d359675efb43f12b04486505ec
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-07-28 12:42:21 +0530

    Time: 76 ms (2.8%), Space: 47.5 MB (6.38%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int cnt=1;
+    int res=0;
+    public int recurr(TreeNode root,int k){
+        //         if(root.left==null && root.right==null){
+        //     if(cnt==k){
+        //         res=root.val;
+        //         // return root.val;
+        //     }
+        //     cnt++;
+        //     return;
+        // }
+        if(root==null){
+            return -1;
+        }
+        int left=recurr(root.left,k);
+        System.out.println(root.val);
+              if(cnt==k){
+            res=root.val;
+        }
+        cnt++;
+         
+        int right=recurr(root.right,k);
+        System.out.println(root.val);
+       if(right!=-1){
+        if(cnt==k){
+            res=root.val;
+        }
+        cnt++;
+       }
+       
+        // if(left==-1 && right==-1){
+        //     if(cnt==k){
+        //         res=root.val;
+        //     }
+        //     cnt++;
+        // }
+        return -1;
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        // if(root.left==null && root.right==null){
+        //     return root.val;
+        // }
+        recurr(root,k);
+        return res;
+    }
+}
