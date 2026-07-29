9557838256aa3658865de0da795707a075625298
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-07-29 22:54:32 +0530

    Time: 64 ms (70.16%), Space: 78.3 MB (43.44%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,18 +1,39 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] arr) {
         Stack<Integer>stk=new Stack<>();
-        int len=heights.length;
-        int max=0;
-        for(int i=0;i<=len;i++){
-            int curr=(i==len)?0:heights[i];
-            while(!stk.isEmpty() && curr<heights[stk.peek()]){
-                int height=heights[stk.pop()];
-                int left=stk.isEmpty()?-1:stk.peek();
-                int right=i;
-                int width=right-left-1;
-                max=Math.max(max,width*height);
+        int len=arr.length;
+        int max=Integer.MIN_VALUE;
+        // stk.push(0);
+        for(int i=0;i<len;i++){
+            // int cnt=1;
+    
+            while(!stk.isEmpty() && arr[stk.peek()]>arr[i]){
+                // max=Math.max(max,stk.pop()*cnt);
+                // stk.push(arr[i]*)
+
+                int curr=stk.pop();
+    
+                int width=(stk.isEmpty())?i*arr[curr]:(i-stk.peek()-1)*arr[curr];
+                max=Math.max(max,width);
             }
-            stk.push(i);
+    
+                stk.push(i);
+            
+         
+        } 
+
+        // while(!stk.isEmpty()){
+
+        // }
+        // int cnt=1;
+        int ind=len;
+        while(!stk.isEmpty()){
+            // max=Math.max(max,stk.pop()*cnt);
+            // cnt++;
+
+            int curr=stk.pop();
+            int width=(stk.isEmpty())?ind*arr[curr]:(ind-stk.peek()-1)*arr[curr];
+            max=Math.max(max,width);
         }
         return max;
     }
