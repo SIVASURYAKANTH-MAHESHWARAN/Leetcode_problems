65211f3bb94293a2e4696cb46fd6dc16fb30f642
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-07-31 16:18:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,6 @@
 class Solution {
-
     public int climbStairs(int n) {
-        if(n==1){
-            return n;
-        }
-        int[]dp=new int[n+1];
+        int[]dp=new int[n+2];
         dp[1]=1;
         dp[2]=2;
         for(int i=3;i<=n;i++){
