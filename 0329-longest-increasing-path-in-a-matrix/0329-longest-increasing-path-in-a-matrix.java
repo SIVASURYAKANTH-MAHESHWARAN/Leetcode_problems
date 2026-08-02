a339a1e3bcc1ff168aae3daf701fd0d9b8845b64
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-08-02 11:37:19 +0530

    Time: 10 ms (28.06%), Space: 47 MB (66.22%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,35 +1,31 @@
 class Solution {
-    int[][]dr={{-1,0},{1,0},{0,1},{0,-1}};
-    public int dfs(int[][]mat,int[][]dp,int i,int j){
-        int ans=1;
-        if(dp[i][j]!=-1){
+    public int dfs(int[][]matrix,int i,int j,Integer[][]dp){
+        if(dp[i][j]!=null){
             return dp[i][j];
         }
-        for(int[]dx:dr){
-            int r=dx[0]+i;
-            int c=dx[1]+j;
-            if(r>=0 && c>=0 && r<mat.length && c<mat[0].length && mat[r][c]>mat[i][j]){
-                ans=Math.max(ans,1+dfs(mat,dp,r,c));
+        int[]dr={-1,1,0,0};
+        int[]dc={0,0,1,-1};
+        int max=0;
+        for(int k=0;k<4;k++){
+            int nr=dr[k]+i;
+            int nc=dc[k]+j;
+            if(nr>=0 && nc>=0 && nr<matrix.length && nc<matrix[0].length && matrix[i][j]<matrix[nr][nc]){
+                max=Math.max(max,dfs(matrix,nr,nc,dp));
             }
-
         }
-        return dp[i][j]=ans;
+        return dp[i][j]=1+max;
     }
     public int longestIncreasingPath(int[][] matrix) {
         int r=matrix.length;
         int c=matrix[0].length;
-        int max=1;
-        int[][]dp=new int[r][c];
+        int ans=0;
+        Integer[][]dp=new Integer[r][c];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                dp[i][j]=-1;
+                int res=dfs(matrix,i,j,dp);
+                ans=Math.max(ans,res);
             }
         }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                max=Math.max(max,dfs(matrix,dp,i,j));
-            }
-        }
-        return max;
+        return ans;
     }
 }
