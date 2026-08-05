bd37eb7445d144d380f2e8c8a63107651c8206f5
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-08-05 22:13:58 +0530

    Time: 1 ms (9.83%), Space: 48.2 MB (22.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,41 +1,42 @@
 class Solution {
     public double myPow(double x, int n) {
-        double num=x;
-        int pow=n;
+        long pow=n;
         double ans=1;
         // if(x<0){
         //     if(x==Integer.MIN_VALUE){
         //         x=I
         //     }
         // }
-        if(n<0){
-            if(n==Integer.MIN_VALUE){
-                n=Integer.MAX_VALUE;
-            }
-            else{
-                n*=-1;
-            }
+        if(pow<0){
+            // if(n==Integer.MIN_VALUE){
+            //     n=Integer.MAX_VALUE;
+            // }
+            // else{
+            //     n*=-1;
+            // }
+            x=1/x;
+            pow*=-1;
         }
-        while(n>0){
-            if(n%2==1){
+        while(pow>0){
+            if(pow%2==1){
                 ans*=x;
             }
             x=x*x;
-            n/=2;
+            pow/=2;
         }
         // if(pow<0 && x<0){
         //     return ans*-1;
         // }
-        if(pow>0){
-            return ans;
-        }
+        // if(pow>0){
+        //     return ans;
+        // }
         // System.out.print(ans);
-        if(num<0){
-            if(pow%2!=0){
-                return (1/ans);
-            }
-            return 1/(ans*-1);
-        }
-        return 1/ans;
+        // if(num<0){
+        //     if(pow%2!=0){
+        //         return (1/ans);
+        //     }
+        //     return 1/(ans*-1);
+        // }
+        return ans;
     }
 }
