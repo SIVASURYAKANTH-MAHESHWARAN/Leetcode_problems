861a1d6e7cb5bc9463a010299c6d502522d3fa1a
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-08-06 22:23:29 +0530

    Time: 48 ms (71.05%), Space: 46.6 MB (70.44%) - LeetHub

---
+++ 4002-count-valid-sequences/4002-count-valid-sequences.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long pow(long a,long b){
+        long ans=1;
+        while(b>0){
+            if(b%2==1){
+                ans=(ans*a)%1000000007;
+            }
+            a=(a*a)%1000000007;
+            b/=2;
+        }
+        return ans%1000000007;
+    }
+    public long mmi(long num){
+        return pow(num,1000000005);
+    }
+    public long comb(int n,int r){
+        long[]fact=new long[n+1];
+        fact[0]=1;
+        // if(n>1){
+        // fact[1]=1;
+        // }
+        for(int i=1;i<=n;i++){
+            fact[i]=(fact[i-1]*i)%1000000007;
+        }
+        System.out.println(n+" "+r);
+        return (fact[n]%1000000007*mmi((fact[n-r]%1000000007*fact[r]%1000000007)%1000000007))%1000000007;
+    }
+    public int countValidSequences(int n, int k) {
+        long total=comb(n-1,k-1);
+        long odd=(n-k)%2==0?comb(((n+k)/2)-1,k-1):0;
+        return (int)(total-odd+1000000007)%1000000007;
+    }
+}
