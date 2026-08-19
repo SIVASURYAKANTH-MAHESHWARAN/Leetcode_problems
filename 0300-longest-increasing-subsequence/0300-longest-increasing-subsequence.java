59ad6f30e792cb422e0216018368df7a05eb1932
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-08-19 13:08:20 +0530

    Time: 48 ms (37.27%), Space: 45.7 MB (98.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,43 @@
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int n=arr.length;
-        int[]dp=new int[n];
-        Arrays.fill(dp,1);
-        for(int i=0;i<n;i++){
-            int max=dp[i];
-           for(int j=i-1;j>=0;j--){
-            if(arr[i]>arr[j]){
-            max=Math.max(max,dp[j]+1);
+    public int lengthOfLIS(int[] nums) {
+     int len=nums.length; 
+     int[]dp=new int[len+1];
+    //  Arrays.fill(dp,1);
+    dp[0]=1;
+     for(int i=1;i<len;i++){
+       
+            int ans=Integer.MIN_VALUE;
+            int ind=i;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    if(dp[j]>ans ){
+                        ans=dp[j];
+                        ind=j;
+                    }
+                }
             }
-           }
-           dp[i]=max;
+            dp[i]=dp[ind]+1;
         }
-        int res=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            res=Math.max(res,dp[i]);
-        }
-        return res;
+        // else{
+        //     // int ind=i;
+        //     // int ans=1;
+        //     // while(ind>0 && nums[ind]>=0){
+        //     //     if(nums[i]>nums[ind]){
+        //     //         ans=dp[ind];
+        //     //         break;
+        //     //     }
+        //     //     ind--;
+        //     // }
+        //     // dp[i]=ans;
+        //     dp[i]=1;
+        // }
+        // for(int i=0;i<len;i++){
+        //     System.out.print(dp[i]+" ");
+        // }
+     int ans=Integer.MIN_VALUE;
+     for(int i=0;i<len;i++){
+        ans=Math.max(ans,dp[i]);
+     }
+     return ans;   
     }
 }
