82909fd689118b8bd1352ff2cb3bbe10b96ada23
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-08-25 18:35:17 +0530

    Time: 50 ms (5.36%), Space: 68.1 MB (7.04%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int solve(int len,int prev,int ind,int sign,int[]nums,int[][][]dp){
+    public int solve(int prev,int ind,int sign,int[]nums,int[][][]dp){
         if(ind==nums.length){
-            return len;
+            return 0;
         }
         if(dp[ind][prev][sign]!=-1){
-            return dp[ind][prev][sign]+len;
+            return dp[ind][prev][sign];
         }
         int take=0;
         int not=0;
@@ -16,19 +16,18 @@ class Solution {
         // }
         if(sign==0){
             if(nums[ind]>nums[prev]){
-                take=solve(len+1,ind,ind+1,1,nums,dp);
+                take=1+solve(ind,ind+1,1,nums,dp);
             }
-            not=solve(len,prev,ind+1,0,nums,dp);
+            not=solve(prev,ind+1,0,nums,dp);
         }
         else{
             if(nums[ind]<nums[prev]){
-                take=solve(len+1,ind,ind+1,0,nums,dp);
+                take=1+solve(ind,ind+1,0,nums,dp);
             }
-            not=solve(len,prev,ind+1,1,nums,dp);
+            not=solve(prev,ind+1,1,nums,dp);
         }
-        int ans=Math.max(take,not);
-        dp[ind][prev][sign]=ans-len;
-        return ans;
+        // int ans=Math.max(take,not);
+        return dp[ind][prev][sign]=Math.max(take,not);
     }
     public int wiggleMaxLength(int[] nums) {
         int ans=0;
@@ -41,7 +40,7 @@ class Solution {
         }
     }
         for(int i=0;i<nums.length;i++){
-            ans=Math.max(ans,Math.max(solve(1,i,i+1,0,nums,dp),solve(1,i,i+1,1,nums,dp)));
+            ans=Math.max(ans,1+Math.max(solve(i,i+1,0,nums,dp),solve(i,i+1,1,nums,dp)));
         }
         return ans;
     }
