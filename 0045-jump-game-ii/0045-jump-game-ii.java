0ae97c4fa2b0ace58a4e6521e9be75525ee7604b
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-09-04 18:04:41 +0530

    Time: 1 ms (99.79%), Space: 47.4 MB (21.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,14 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int far=0;
-        int currend=0;
         int steps=0;
+        int mr=0;
+        int pm=0;
         int n=nums.length;
         for(int i=0;i<n-1;i++){
-            far=Math.max(far,i+nums[i]);
-            if(i==currend){
+            pm=Math.max(pm,i+nums[i]);     
+            if(i==mr){
                 steps++;
-                currend=far;
+                // mr=Math.max(mr,i+nums[i]);
+                mr=pm;
             }
         }
         return steps;
