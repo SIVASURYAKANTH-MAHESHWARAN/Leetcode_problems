93077b60fc426344524ce1d4cb934ef4e25b3f89
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-09-12 09:24:28 +0530

    Time: 65 ms (39.89%), Space: 47.5 MB (67.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,18 +5,16 @@ class Solution {
         int low=0;
         int n=s.length();
         for(int i=0;i<n;i++){
-            if(set.contains(s.charAt(i))){
+          
                 while(set.contains(s.charAt(i))){
                     set.remove(s.charAt(low));
                     low++;
                 }
                 set.add(s.charAt(i));
-            }
-            else{
-                set.add(s.charAt(i));
-                // System.out.println(i+" "+low+" "+1);
+            
+            
                 max=Math.max(max,i-low+1);
-            }
+            
         }
         return max;
     }
