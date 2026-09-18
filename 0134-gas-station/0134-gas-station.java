5589ed9a6049df8bead7f091406ff43ae33fd0c9
SIVASURYAKANTH MAHESHWARAN <sivasuryakanth1@gmail.com>
2026-09-18 13:54:41 +0530

    Time: 2 ms (99.18%), Space: 119.7 MB (10.14%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,20 +1,59 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int tc=0;
-        int cc=0;
-        int n=gas.length;
-        int ind=0;
+          int n=gas.length;
+        int tcost=0;
+        int tgas=0;
         for(int i=0;i<n;i++){
-            tc+=gas[i]-cost[i];
-            cc+=gas[i]-cost[i];
-            if(cc<0){
-                ind=i+1;
-                cc=0;
-            }
+            tcost+=gas[i];
+            tgas+=cost[i];
         }
-        if(tc<0){
+        if(tcost<tgas){
             return -1;
         }
-        return ind;
+        // for(int i=0;i<n;i++){
+        //     int g=gas[i];
+        //     boolean flag=true;
+        //     for(int j=i;j<=i+n;j++){
+        //         if(i==j){
+        //             if(g<cost[i]){
+        //                 flag=false;
+        //                 break;
+        //             }
+        //         }
+        //         else{
+        //             if(g-cost[(j-1)%n]<0){
+        //                 flag=false;
+        //                 break;
+        //             }
+        //             g+=gas[j%n]-cost[(j-1)%n];
+        //         }
+        //         // else{
+        //         // ggas[j%n]-cost[(j-1)%n];
+        //         // System.out.print(g);
+        //         // }
+        //         // if(g<0){
+        //         //     break;
+        //         // }
+        //     }
+        //     if(flag){
+        //     return i;
+        //     }
+        // }
+        // int tcos=0;
+        tgas=gas[0];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int g=(i==0)?0:gas[i];
+            int c=(i==0)?0:cost[i-1];
+            int rem=tgas-c;
+            if(rem<0){
+                tgas=0;
+                c=0;
+                ans=i;
+            }
+            tgas+=g-c;
+            // System.out.println(tgas);
+        }
+        return ans;
     }
 }
